31cb13658aeb1922343706dbb7a6b3f31e1d66a2
Muhammed-200023 <272790@ust.com>
2024-09-26 12:52:34 +0530

    Delete src directory

--- src/main/java/com/ust/sustainability/service/LookupService.java
+++
@@ -1,9 +0,0 @@
-package com.ust.sustainability.service;
-
-import com.ust.sustainability.model.Lookup;
-
-import java.util.List;
-
-public interface LookupService {
-    List<Lookup> getAllLookups();
-}
--- src/main/java/com/ust/sustainability/service/ReportConfigService.java
+++
@@ -1,9 +0,0 @@
-package com.ust.sustainability.service;
-
-import com.ust.sustainability.model.ReportConfig;
-
-import java.util.List;
-
-public interface ReportConfigService {
-    List<ReportConfig> getAllReportConfigs();
-}
